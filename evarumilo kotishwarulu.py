55ba2fc2d9a3ad07b465581e16e96760088d1684
raviram9902 <55321106+raviram9902@users.noreply.github.com>
2025-07-11 20:50:57 -0400

    Add files via upload

---
+++ evarumilo kotishwarulu.py
@@ -0,0 +1,23 @@
+questions = [
+    ["What is the capital of India?", "Delhi"],
+    ["Which planet is known as the Red Planet?", "Mars"],
+    ["How many states are there in India?", "28"],
+    ["Who wrote the national anthem of India?", "Rabindranath Tagore"]
+]
+
+Moneyearned = [100, 1000, 10000, 100000]
+winnings = 0
+
+print(" Welcome to Evaru Meelo Kotiswarulu \n")
+
+for i in range(len(questions)):
+    print(f"Q{i+1}: {questions[i][0]}")
+    useranswer = input("Enter your answer: ")
+    if useranswer.strip().lower() == questions[i][1].lower():
+        winnings = Moneyearned[i]
+        print(f" Correct! You won ₹{Moneyearned[i]}\n")
+    else:
+        print(" Wrong answer!")
+        break
+
+print(f"\n🏆 You take home ₹{winnings}")
